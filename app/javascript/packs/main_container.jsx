883493e9a526ef7f011a463f4a3a058700c69c04
dracco1993 <dracco1993@gmail.com>
2020-01-03 21:13:22 -0500

    Working React/webpacker/webpack/babel setup

---
+++ app/javascript/packs/main_container.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const MainContainer = props => <div>Hello {props.name}!</div>;
+
+MainContainer.defaultProps = {
+  name: "David"
+};
+
+MainContainer.propTypes = {
+  name: PropTypes.string
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <MainContainer name="React" />,
+    document
+      .getElementById("main-container")
+      .appendChild(document.createElement("div"))
+  );
+});
