5e21a4b9156d6e8ae6b53f2f3f95b5247dad399a
dracco1993 <dracco1993@gmail.com>
2020-01-07 02:09:29 -0500

    'working' WIP on API user auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
   # GET /users
@@ -29,10 +29,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -43,10 +41,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
-        format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -57,7 +53,6 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -71,11 +66,8 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(
-        :username,
         :email,
-        :password,
-        :salt,
-        :encrypted_password
+        :password
       )
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :lockable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+  def generate_jwt
+    JWT.encode({
+        id: id,
+        exp: 7.days.from_now.to_i
+      },
+      Rails.application.secrets.secret_key_base
+    )
+  end
 end
