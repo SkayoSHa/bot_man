8d9c09673618ecbe1a1d55cbfb78ed02737d7293
dracco1993 <dracco1993@gmail.com>
2020-01-19 16:39:59 -0500

    Clean up BaseCommandContainer to use ContainerWrapper

--- lib/bot/commands/base_command_container.rb
+++ lib/bot/commands/base_command_container.rb
@@ -1,27 +1,11 @@
 # frozen_string_literal: true
 
-class BaseCommandContainer
+class BaseCommandContainer < ContainerWrapper
   extend Discordrb::Commands::CommandContainer
 
   # Overwrite command, so that we can record the
   # user that's triggering it in the database
   def self.command(name, attributes = {}, &block)
-
-    # Wrap the original proc
-    wrapped_proc = Proc.new do |event|
-      # Handle the event with whatever extra
-      # we want to do
-      handle_event(event)
-
-      # Call the original proc passed into #command
-      block.call(event)
-    end
-
-    # Call the original #command
-    super(name, attributes = {}, &wrapped_proc)
-  end
-
-  def self.handle_event(event)
-    UserService.update_db_from_user(event.user)
+    super(name, attributes, &proc_wrapper(&block))
   end
 end
