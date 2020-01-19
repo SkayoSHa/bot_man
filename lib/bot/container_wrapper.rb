36de50f30bdb153e2ddb6e5d02b70fd9b8fab3c4
dracco1993 <dracco1993@gmail.com>
2020-01-19 17:50:19 -0500

    Fix passing more than one arg

--- lib/bot/container_wrapper.rb
+++ lib/bot/container_wrapper.rb
@@ -3,17 +3,17 @@
 class ContainerWrapper
   def self.proc_wrapper(&block)
     # Wrap the original proc
-    Proc.new do |event|
+    Proc.new do |event, *args|
       # Handle the event with whatever extra
       # we want to do
-      handle_event(event)
+      handle_event(event, args)
 
       # Call the original proc passed into #command
-      block.call(event)
+      block.call(event, *args)
     end
   end
 
-  def self.handle_event(event)
+  def self.handle_event(event, args)
     UserService.update_db_from_user(event.user)
   end
 end
