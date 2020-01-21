02c0642e3781f7403d01bec84de91f2d7ba58f78
dracco1993 <dracco1993@gmail.com>
2020-01-20 23:36:02 -0500

    Added wuote playback

--- lib/bot/commands/quotes_container.rb
+++ lib/bot/commands/quotes_container.rb
@@ -25,4 +25,36 @@ class QuotesContainer < BaseCommandContainer
     # TODO: link the id returned to the frontend?
     "Quote ##{new_quote.id} successfully added"
   end
+
+  command :quote, aliases: [:quotes], min_args: 0, max_args: 1, description: "Replays a quote", usage: 'quote (@<user>|<id>)' do |event, target|
+    # Return a random one, if target is nil
+    return format_quote(random_quote) if target.nil?
+
+    key = target.delete("^0-9")
+
+    target_user = event.server.member(key)
+
+    # There was a user supplied
+    if target_user
+      return format_quote(random_quote(user: target_user))
+    end
+
+    quote = Quote.find_by_id(key)
+    return format_quote(quote)
+  end
+end
+
+def random_quote(user: nil)
+  quote = Quote
+
+  if user
+    quote = quote.where(quotee_uid: user.id)
+  end
+
+  quote.order("RANDOM()").first
+end
+
+def format_quote(quote)
+  return "No quote found." unless quote
+  quote.quote
 end
