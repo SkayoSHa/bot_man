84a48cd564603050d024053f28092be7620054e2
dracco1993 <dracco1993@gmail.com>
2020-03-20 14:34:27 -0400

    Actually annotate things

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: quotes
+#
+#  id         :bigint           not null, primary key
+#  quote      :string
+#  quotee_uid :bigint
+#  quoter_uid :bigint
+#  server_uid :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  message_id :bigint
+#
 class Quote < ApplicationRecord
 end
