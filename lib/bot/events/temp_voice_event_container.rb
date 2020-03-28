1c85d4905f5fd68d1b6538eb1f9b7751e7c8002f
dracco1993 <dracco1993@gmail.com>
2020-03-28 16:51:48 -0400

    Move user to new temp channel

--- lib/bot/events/temp_voice_event_container.rb
+++ lib/bot/events/temp_voice_event_container.rb
@@ -7,9 +7,11 @@ class TempVoiceEventContainer < BaseEventContainer
     # Don't do anything if they're leaving the channel
     next unless event.channel
 
+    server = event.server
+
     # If it's a jump channel
     jump_channel = TemporaryVoiceChannel.where(
-      server_uid: event.server.id,
+      server_uid: server.id,
       channel_uid: event.channel.id,
       is_jump_channel: true,
       active: true
@@ -19,10 +21,10 @@ class TempVoiceEventContainer < BaseEventContainer
 
     # Find the actual reference to the discord channel
     discord_jump_channel =
-      event.server.voice_channels.detect { |channel| channel.id == event.channel.id }
+      server.voice_channels.detect { |channel| channel.id == event.channel.id }
 
     # Make the new channel
-    new_channel = event.server.create_channel(
+    new_channel = server.create_channel(
       "~#{event.user.name}",
       2, # type
       reason: "Creating temporary voice channel"
@@ -33,11 +35,14 @@ class TempVoiceEventContainer < BaseEventContainer
 
     # Record information to DB
     TemporaryVoiceChannel.create!(
-      server_uid: event.server.id,
+      server_uid: server.id,
       creator_uid: event.user.id,
       channel_uid: new_channel.id,
       is_jump_channel: false,
       active: true
     )
+
+    # Move the user to the new channel
+    server.move(event.user, new_channel)
   end
 end
