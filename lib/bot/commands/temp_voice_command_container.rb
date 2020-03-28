a363b6b395db239af61a14005b29b2e0d484d7af
dracco1993 <dracco1993@gmail.com>
2020-03-28 18:53:09 -0400

    Added deletejumpchannel command

--- lib/bot/commands/temp_voice_command_container.rb
+++ lib/bot/commands/temp_voice_command_container.rb
@@ -6,14 +6,12 @@ class TempVoiceCommandContainer < BaseCommandContainer
   command(
     :createjumpchannel,
     min_args: 1,
-    max_args: 1,
     description: "Creates a new temporary voice jump channel",
     usage: "createjumpchannel [jump channel name]",
     help_available: false
-  ) do |event, jump_channel_name|
+  ) do |event, *jump_channel_array|
+    jump_channel_name = jump_channel_array.join(" ")
     return "Please supply a jump channel name" unless jump_channel_name
-
-    UserService.ensure_user(event.user)
 
     # Make sure that the user is an admin
     break unless event.user.permission?(:administrator)
@@ -36,6 +34,44 @@ class TempVoiceCommandContainer < BaseCommandContainer
       active: true
     )
 
-    "Temporary jump channel \"#{jump_channel_name}\" created"
+    "Temporary jump channel `#{jump_channel_name}` created"
+  end
+
+  command(
+    :deletejumpchannel,
+    min_args: 1,
+    description: "Deletes a temporary voice jump channel",
+    usage: "deletejumpchannel [jump channel name]",
+    help_available: false
+  ) do |event, *jump_channel_array|
+    jump_channel_name = jump_channel_array.join(" ")
+    return "Please supply a jump channel name" unless jump_channel_name
+
+    # Make sure that the user is an admin
+    break unless event.user.permission?(:administrator)
+
+    server = event.server
+
+    # Find the actual reference to the discord channel
+    discord_jump_channel =
+      server.voice_channels.detect { |c| c.name == jump_channel_name }
+
+    # If it's a jump channel
+    jump_channel = TemporaryVoiceChannel.where(
+      server_uid: server.id,
+      channel_uid: discord_jump_channel.id,
+      is_jump_channel: true,
+      active: true
+    ).first
+
+    return unless jump_channel
+
+    # Mark the channel as inactive
+    jump_channel.update!(active: false)
+
+    # Delete the channel
+    discord_jump_channel.delete
+
+    "Temporary jump channel `#{jump_channel_name}` deleted"
   end
 end
