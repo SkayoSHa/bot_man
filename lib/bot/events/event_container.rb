9ec8cc11df03e795c3212e161dd8a24d0081bf15
dracco1993 <dracco1993@gmail.com>
2020-03-31 00:07:09 -0400

    Cleaned up event listening

--- lib/bot/events/event_container.rb
+++ lib/bot/events/event_container.rb
@@ -2,8 +2,6 @@
 
 class EventContainer < BaseEventContainer
   raw do |event|
-    # pp event.type
-
     # https://discordapp.com/developers/docs/topics/gateway#commands-and-events
     Event.create!(
       type: "Events::#{event.type.to_s.downcase.classify}Event",
