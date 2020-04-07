72ac0a9283f75c8e190d91513250609c857a81fd
dracco1993 <dracco1993@gmail.com>
2020-04-06 22:40:25 -0400

    Add sidekiq UI route things

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Authenticated routes
+  require "sidekiq/web"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end
+  mount Sidekiq::Web, at: "/sidekiq"
+
+  # At some point, change sidekiq auto to do something like this:
+  # authenticate :user, ->(u) { u.has_role?(:admin) } do
+  #   mount Sidekiq::Web, at: "/sidekiq"
+  # end
+
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: {
       sessions: :sessions,
