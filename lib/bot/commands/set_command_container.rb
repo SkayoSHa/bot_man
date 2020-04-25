30072b60acf100aa126edc8ee6650a162a058787
dracco1993 <dracco1993@gmail.com>
2020-04-25 16:58:06 -0400

    Add base set region implementation

---
+++ lib/bot/commands/set_command_container.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class SetCommandContainer < BaseCommandContainer
+  extend Discordrb::EventContainer
+
+  command(
+    :set,
+    min_args: 1,
+    description: "Updates a bot/server setting",
+    usage: "set [setting] [additional options]",
+    help_available: true
+  ) do |event, setting, *options|
+    return "Please supply a setting" unless setting
+
+    # Make sure that the user is an admin
+    return "Permission denied." unless event.user.permission?(:administrator)
+
+    response = ""
+
+    case setting
+    when "region"
+      response = set_server_region(event, options)
+    else
+      return "Invalid setting."
+    end
+
+    return response
+  end
+
+  def self.set_server_region(event, options)
+    available_regions = event.server.available_voice_regions
+    # If no options, display server regions
+    # unless options.empty?
+    #   if options
+        event.server.region = options.first
+    #   end
+    # end
+
+    # binding.pry
+  end
+end
