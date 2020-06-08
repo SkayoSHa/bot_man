889a89816a75a82f0a90f289ff76b149ac010765
dracco1993 <dracco1993@gmail.com>
2020-06-08 14:13:09 -0400

    Auth user for servers_controller

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ServersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_server, only: [:timeline]
 
   def index
