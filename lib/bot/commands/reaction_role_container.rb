346050986912b2e4c32df168d2b9d7f84d745326
dracco1993 <dracco1993@gmail.com>
2020-06-19 15:14:07 -0400

    Add base addreactionrole command

---
+++ lib/bot/commands/reaction_role_container.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class ReactionRoleContainer < BaseCommandContainer
+  extend Discordrb::EventContainer
+
+  command :addreactionrole,
+          aliases: [:arr],
+          min_args: 3,
+          max_args: 3,
+          description: "Add a new reaction role to a message",
+          usage: "addreactionrole <message_id> :emoji: <role_id>" do |event, message_id, emoji, role_id|
+    # Validate message actually exists
+    message = event.channel.load_message(message_id)
+    return "Please supply a message_id from this channel" unless message
+
+    # TODO: only allow emoji from this server/generally available
+
+    # TODO: only allow valid roles
+
+    # Actually add the new reaction role
+    ReactionRole.create!(
+      message_id: message_id,
+      reaction: emoji,
+      role_id: role_id
+    )
+
+    return "\"#{role_id}\" sucessfully linked to #{emoji} for \"#{message_id}\""
+  end
+end
